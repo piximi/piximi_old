032d6ffbf82b8ea53c5e9e499faba08b699b098e
Allen Goodman <allen.goodman@icloud.com>
2019-05-10 14:21:13 -0400

    style

--- packages/@piximi/store/src/actions/classifier.ts
+++ packages/@piximi/store/src/actions/classifier.ts
@@ -2,23 +2,37 @@ import {
   createAction
 } from "redux-starter-kit";
 
-export const createCategoryAction = createAction('create-category');
+export const createCategoryAction = createAction(
+  'create-category'
+);
 
-export const createClassifierAction = createAction('create-classifier');
+export const createClassifierAction = createAction(
+  'create-classifier'
+);
 
-export const createImageAction = createAction('create-image');
+export const createImageAction = createAction(
+  'create-image'
+);
 
-export const createImageScoreAction = createAction('create-image-score');
+export const createImageScoreAction = createAction(
+  'create-image-score'
+);
 
-export const deleteCategoryAction = createAction('delete-category');
+export const deleteCategoryAction = createAction(
+  'delete-category'
+);
 
-export const deleteImageAction = createAction('delete-image');
+export const deleteImageAction = createAction(
+  'delete-image'
+);
 
 export const toggleCategoryVisibilityAction = createAction(
   'toggle-category-visibility'
 );
 
-export const updateCategoryColorAction = createAction('update-category-color');
+export const updateCategoryColorAction = createAction(
+  'update-category-color'
+);
 
 export const updateCategoryDescriptionAction = createAction(
   'update-category-description'
@@ -36,9 +50,13 @@ export const updateImageBrightnessAction = createAction(
   'update-image-brightness'
 );
 
-export const updateImageCategoryAction = createAction('update-image-category');
+export const updateImageCategoryAction = createAction(
+  'update-image-category'
+);
 
-export const updateImageContrastAction = createAction('update-image-contrast');
+export const updateImageContrastAction = createAction(
+  'update-image-contrast'
+);
 
 export const updateImageVisibilityAction = createAction(
   'update-image-visibility'
