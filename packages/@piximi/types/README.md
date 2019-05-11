eb444461ba816354bb8b39ebd54197279a06c2af
Allen Goodman <allen.goodman@icloud.com>
2019-05-11 16:50:50 -0400

    0.1.0

---
+++ packages/@piximi/types/README.md
@@ -0,0 +1,3 @@
+# @cytoai/types
+
+[![Build Status](https://travis-ci.org/cytoai/types.svg?branch=master)](https://travis-ci.org/cytoai/types)
---
+++ packages/@piximi/types/src/index.ts
@@ -0,0 +1,51 @@
+export type Blob = {
+  checksum: string;
+}
+
+export type Category = {
+  color: string;
+  description: string;
+  identifier: string;
+  index: number;
+  visible: boolean;
+};
+
+export type Classifier = {
+  categories: Category[];
+  images: Image[];
+  name: string;
+};
+
+export type Image = {
+  categoryIdentifier: string;
+  checksum: string;
+  data: string;
+  identifier: string;
+  partition: Partition;
+  scores: Score[];
+  visualization: ImageVisualization;
+};
+
+export enum Partition {
+  Training,
+  Validation,
+  Test
+}
+
+export type Score = {
+  categoryIdentifier: string;
+  probability: number;
+};
+
+export type Settings = {
+  spinner: {
+    spinning: boolean;
+  };
+};
+
+export type ImageVisualization = {
+  brightness: number;
+  visibleChannels: number[];
+  contrast: number;
+  visible: boolean;
+};
