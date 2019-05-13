20082203f40d0a2da0a5246aa27be4406d7eaf6e
Allen Goodman <allen.goodman@icloud.com>
2019-05-13 19:44:03 -0400

    localforage

--- packages/@piximi/store/src/store.ts
+++ packages/@piximi/store/src/store.ts
@@ -5,7 +5,7 @@ import {
 } from 'redux-persist'
 import thunk from 'redux-thunk'
 import autoMergeLevel2 from "redux-persist/es/stateReconciler/autoMergeLevel2";
-import storage from 'redux-persist/lib/storage'
+import storage from "localforage"
 
 import {
   AnyAction,
