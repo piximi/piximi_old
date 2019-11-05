2e5c707e49c9a792a0be558a1c53f060b047fad1
Allen Goodman <allen.goodman@icloud.com>
2019-11-05 12:52:08 -0500

    image-dialog

--- packages/@piximi/image-dialog/src/index.ts
+++ packages/@piximi/image-dialog/src/index.ts
@@ -1,9 +1,7 @@
-export { BrightnessSlider } from './BrightnessSlider/BrightnessSlider';
-export { ChannelSelection } from './ChannelSelection/ChannelSelection';
-export { ContrastSlider } from './ContrastSlider/ContrastSlider';
-export { ImageHistogram } from './ImageHistogram/ImageHistogram';
+export { BrightnessSlider } from './BrightnessSlider';
+export { ChannelSelection } from './ChannelSelection';
+export { ContrastSlider } from './ContrastSlider';
+export { ImageHistogram } from './ImageHistogram';
 export { ImageViewer } from './ImageViewer/ImageViewer';
-export { ImageViewerDialog } from './ImageViewerDialog/ImageViewerDialog';
-export {
-  ImageViewerExposureDrawer
-} from './ImageViewerExposureDrawer/ImageViewerExposureDrawer';
+export { ImageViewerDialog } from './ImageViewerDialog';
+export { ImageViewerExposureDrawer } from './ImageViewerExposureDrawer';
