736c2f239858851f92a5104a1cddad593ed83139
Allen Goodman <allen.goodman@icloud.com>
2019-11-06 11:39:54 -0500

    stories and tests

---
+++ packages/@piximi/image-dialog/src/ChannelSelection/ChannelSelection.test.tsx
@@ -0,0 +1,12 @@
+import { shallow } from 'enzyme';
+import * as React from 'react';
+
+it('Example', () => {
+  const open = true;
+
+  const onClose = () => {};
+
+  const e = <div />;
+
+  shallow(e);
+});
