6bfe167b6be8391896900710d16912c226756c69
Allen Goodman <allen.goodman@icloud.com>
2019-11-06 13:39:59 -0500

    stories and tests

--- packages/@piximi/image-dialog/src/ImageViewer/ImageViewer.stories.tsx
+++ packages/@piximi/image-dialog/src/ImageViewer/ImageViewer.stories.tsx
@@ -2,7 +2,22 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { ConnectedImageViewer } from './ConnectedImageViewer';
+import { Provider } from 'react-redux';
+import { store } from '@piximi/store';
+import { ImageViewer } from './ImageViewer';
+
+const src: string = 'https://dummyimage.com/512x512/f3f3f3/fff.png&text=+';
+
+const onClose = () => {};
 
 storiesOf('ImageViewer', module).add('ImageViewer', () => {
-  return <ConnectedImageViewer />;
+  return (
+    <Provider store={store}>
+      <ConnectedImageViewer
+        imgIdentifier={'example'}
+        src={src}
+        onClose={onClose}
+      />
+    </Provider>
+  );
 });
