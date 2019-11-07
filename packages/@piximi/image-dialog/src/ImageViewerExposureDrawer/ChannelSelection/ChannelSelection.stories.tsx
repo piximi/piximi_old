870c8ee155ca35275b60d354a693b10542e3bc4f
Allen Goodman <allen.goodman@icloud.com>
2019-11-07 14:25:50 -0500

    channel selection

--- packages/@piximi/image-dialog/src/ImageViewerExposureDrawer/ChannelSelection/ChannelSelection.stories.tsx
+++ packages/@piximi/image-dialog/src/ImageViewerExposureDrawer/ChannelSelection/ChannelSelection.stories.tsx
@@ -2,11 +2,43 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { ChannelSelection } from './ChannelSelection';
+import {
+  Checkbox,
+  IconButton,
+  ListItem,
+  ListItemIcon,
+  ListItemSecondaryAction,
+  ListItemText
+} from '@material-ui/core';
+import CommentIcon from '@material-ui/icons/Comment';
+import List from '@material-ui/core/List';
 
 storiesOf('ChannelSelection', module).add('ChannelSelection', () => {
   return (
-    <ChannelSelection>
-      <div />
-    </ChannelSelection>
+    <List component="nav" aria-label="main mailbox folders">
+      <ListItem role={undefined} dense button onClick={() => {}}>
+        <ListItemIcon>
+          <Checkbox edge="start" checked={true} tabIndex={-1} disableRipple />
+        </ListItemIcon>
+        <ListItemText id={''} primary={`Line item`} />
+        <ListItemSecondaryAction>
+          <IconButton edge="end" aria-label="comments">
+            <CommentIcon />
+          </IconButton>
+        </ListItemSecondaryAction>
+      </ListItem>
+
+      <ListItem role={undefined} dense button onClick={() => {}}>
+        <ListItemIcon>
+          <Checkbox edge="start" checked={true} tabIndex={-1} disableRipple />
+        </ListItemIcon>
+        <ListItemText id={''} primary={`Line item`} />
+        <ListItemSecondaryAction>
+          <IconButton edge="end" aria-label="comments">
+            <CommentIcon />
+          </IconButton>
+        </ListItemSecondaryAction>
+      </ListItem>
+    </List>
   );
 });
---
+++ packages/@piximi/image-dialog/src/ImageViewerExposureDrawer/ChannelSelection/ChannelSelection.tsx
@@ -0,0 +1,128 @@
+import React, { useState } from 'react';
+import Checkbox from '@material-ui/core/Checkbox';
+import List from '@material-ui/core/List';
+import {
+  IconButton,
+  ListItem,
+  ListItemIcon,
+  ListItemSecondaryAction,
+  ListItemText
+} from '@material-ui/core';
+import CommentIcon from '@material-ui/core/SvgIcon/SvgIcon';
+import { green } from '@material-ui/core/colors';
+import { red } from '@material-ui/core/colors';
+import { blue } from '@material-ui/core/colors';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import { withStyles } from '@material-ui/core/styles';
+
+const GreenCheckbox = withStyles({
+  root: {
+    color: green[400],
+    '&$checked': {
+      color: green[600]
+    }
+  },
+  checked: {}
+})(props => <Checkbox color="default" {...props} />);
+
+const RedCheckbox = withStyles({
+  root: {
+    color: red[400],
+    '&$checked': {
+      color: red[600]
+    }
+  },
+  checked: {}
+})(props => <Checkbox color="default" {...props} />);
+
+const BlueCheckbox = withStyles({
+  root: {
+    color: blue[400],
+    '&$checked': {
+      color: blue[600]
+    }
+  },
+  checked: {}
+})(props => <Checkbox color="default" {...props} />);
+
+export const ChannelSelection = (props: any) => {
+  const [greenCheckboxChecked, setGreenCheckboxChecked] = useState(true);
+
+  const onGreenCheckboxChange = () => {
+    setGreenCheckboxChecked(!greenCheckboxChecked);
+  };
+  const [redCheckboxChecked, setRedCheckboxChecked] = useState(true);
+
+  const onRedCheckboxChange = () => {
+    setRedCheckboxChecked(!redCheckboxChecked);
+  };
+  const [blueCheckboxChecked, setBlueCheckboxChecked] = useState(true);
+
+  const onBlueCheckboxChange = () => {
+    setBlueCheckboxChecked(!blueCheckboxChecked);
+  };
+
+  return (
+    <List component="nav" aria-label="main mailbox folders">
+      <ListItem role={undefined} dense button onClick={() => {}}>
+        <ListItemIcon>
+          <FormControlLabel
+            control={
+              <RedCheckbox
+                checked={redCheckboxChecked}
+                onChange={onRedCheckboxChange}
+                value="checkedG"
+              />
+            }
+            label="Red"
+          />
+        </ListItemIcon>
+        <ListItemSecondaryAction>
+          <IconButton edge="end" aria-label="comments">
+            <CommentIcon />
+          </IconButton>
+        </ListItemSecondaryAction>
+      </ListItem>
+
+      <ListItem role={undefined} dense button onClick={() => {}}>
+        <ListItemIcon>
+          <FormControlLabel
+            control={
+              <GreenCheckbox
+                checked={greenCheckboxChecked}
+                onChange={onGreenCheckboxChange}
+                value="checkedG"
+              />
+            }
+            label="Green"
+          />
+        </ListItemIcon>
+        <ListItemSecondaryAction>
+          <IconButton edge="end" aria-label="comments">
+            <CommentIcon />
+          </IconButton>
+        </ListItemSecondaryAction>
+      </ListItem>
+
+      <ListItem role={undefined} dense button onClick={() => {}}>
+        <ListItemIcon>
+          <FormControlLabel
+            control={
+              <BlueCheckbox
+                checked={blueCheckboxChecked}
+                onChange={onBlueCheckboxChange}
+                value="checkedG"
+              />
+            }
+            label="Blue"
+          />
+        </ListItemIcon>
+        <ListItemSecondaryAction>
+          <IconButton edge="end" aria-label="comments">
+            <CommentIcon />
+          </IconButton>
+        </ListItemSecondaryAction>
+      </ListItem>
+    </List>
+  );
+};
