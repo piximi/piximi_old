4fb042fdf98212650c91e8f33d1d885b99ebde4b
Allen Goodman <allen.goodman@icloud.com>
2019-11-07 10:15:19 -0500

    stories and tests

--- packages/@piximi/image-dialog/src/ImageViewerExposureDrawer/ImageViewerExposureDrawer.stories.tsx
+++ packages/@piximi/image-dialog/src/ImageViewerExposureDrawer/ImageViewerExposureDrawer.stories.tsx
@@ -2,10 +2,30 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { ImageViewerExposureDrawer } from './ImageViewerExposureDrawer';
+import * as example from '../Image/example.jpg';
+
+const setBrightness = () => {};
+
+const setContrast = () => {};
+
+const setUnselectedChannels = () => {};
 
 storiesOf('ImageViewerExposureDrawer', module).add(
   'ImageViewerExposureDrawer',
   () => {
-    return <ImageViewerExposureDrawer />;
+    return (
+      <ImageViewerExposureDrawer
+        onClose={() => {}}
+        open={true}
+        src={example}
+        imgIdentifier={''}
+        setBrightness={setBrightness}
+        brightness={100}
+        setContrast={setContrast}
+        contrast={100}
+        setUnselectedChannels={setUnselectedChannels}
+        unselectedChannels={[]}
+      />
+    );
   }
 );
