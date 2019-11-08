3b4da03ee7ce96bd93a3a36cfb7f4627e7150aaa
Allen Goodman <allen.goodman@icloud.com>
2019-11-08 11:27:16 -0500

    fixed large button

--- packages/@piximi/image-dialog/src/ImageViewer/ImageViewer.tsx
+++ packages/@piximi/image-dialog/src/ImageViewer/ImageViewer.tsx
@@ -1,10 +1,6 @@
 import * as React from 'react';
-import styles from './ImageViewer.css';
 import { ImageViewerAppBar, ImageViewerExposureDrawer } from '..';
 import { ImageCanvas } from '..';
-import { makeStyles } from '@material-ui/styles';
-
-const useStyles = makeStyles(styles);
 
 type ImageViewerProps = {
   src: string;
@@ -12,17 +8,15 @@ type ImageViewerProps = {
 };
 
 export const ImageViewer = (props: ImageViewerProps) => {
-  const classes = useStyles({});
-
   const { src, onClose } = props;
 
   return (
     <>
       <ImageCanvas src={src} />
 
-      <ImageViewerAppBar onClose={onClose} />
-
       <ImageViewerExposureDrawer src={src} />
+
+      <ImageViewerAppBar onClose={onClose} />
     </>
   );
 };
