bcf0431e9aeaeaa47ed3d6b225ba8fa87a42b09a
Allen Goodman <allen.goodman@icloud.com>
2019-12-03 12:48:11 -0500

    dexie

--- packages/@piximi/upload-image-dialog/src/UploadImageDialog/UploadImageDialog/UploadImageDialog.tsx
+++ packages/@piximi/upload-image-dialog/src/UploadImageDialog/UploadImageDialog/UploadImageDialog.tsx
@@ -1,43 +1,97 @@
 import '@uppy/core/dist/style.css';
 import '@uppy/dashboard/dist/style.css';
-import '@uppy/url/dist/style.css';
-import '@uppy/url/dist/style.css';
-import '@uppy/webcam/dist/style.css';
 import * as React from 'react';
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import Dropbox from '@uppy/dropbox';
-import GoogleDrive from '@uppy/google-drive';
 import Uppy from '@uppy/core';
-import Url from '@uppy/url';
-import Webcam from '@uppy/webcam';
 import { Dashboard } from '@uppy/react';
+import Dexie from 'dexie';
+
+interface ImageEntry {
+  id?: number;
+  data: Blob;
+}
+
+class ImageDatabase extends Dexie {
+  images: Dexie.Table<ImageEntry, number>;
+
+  constructor() {
+    super('ImageDatabase');
+
+    const schema = {
+      images: '++id, data'
+    };
+
+    this.version(1).stores(schema);
+
+    this.images = this.table('images');
+  }
+}
+
+class IndexedDB extends Uppy.Plugin {
+  id: string;
+  type: string;
+
+  constructor(uppy: Uppy.Uppy, options: any) {
+    super(uppy, options);
+
+    this.id = options.id || 'IndexedDB';
+    this.type = 'uploader';
+
+    this.preprocess = this.preprocess.bind(this);
+    this.upload = this.upload.bind(this);
+    this.postprocess = this.postprocess.bind(this);
+  }
+
+  preprocess(identifiers: Array<string>) {
+    return Promise.resolve();
+  }
+
+  upload(identifiers: Array<string>) {
+    const image = this.uppy.getFile(identifiers[0]);
+
+    debugger;
+
+    const imageDatabase = new ImageDatabase();
+
+    const promises = identifiers.map(identifier => {
+      const data: Blob = this.uppy.getFile(identifier).data;
+
+      return imageDatabase.images.add({
+        data: data
+      });
+    });
+
+    return Promise.all(promises);
+  }
+
+  postprocess(identifiers: Array<string>) {
+    return Promise.resolve();
+  }
+
+  install() {
+    this.uppy.addPreProcessor(this.preprocess);
+    this.uppy.addUploader(this.upload);
+    this.uppy.addPostProcessor(this.upload);
+  }
+
+  uninstall() {
+    this.uppy.removePreProcessor(this.preprocess);
+    this.uppy.removeUploader(this.upload);
+    this.uppy.removePostProcessor(this.postprocess);
+  }
+}
+
+const uppy = new Uppy.Uppy();
+
+uppy.use(IndexedDB, {});
+
+const plugins: Array<string> = ['IndexedDB'];
 
 type UploadImageDialogProps = {
   closeDialog: () => void;
   openedDialog: boolean;
 };
-
-const uppy = Uppy();
-
-uppy.use(Dropbox, {
-  companionUrl: 'https://companion.uppy.io/',
-  locale: {}
-});
-
-uppy.use(GoogleDrive, {
-  companionUrl: 'https://companion.uppy.io/',
-  locale: {}
-});
-
-uppy.use(Url, {
-  companionUrl: 'https://companion.uppy.io/',
-  locale: {}
-});
-
-uppy.use(Webcam, {});
-
-const plugins = ['Dropbox', 'GoogleDrive', 'Url', 'Webcam'];
 
 export const UploadImageDialog = ({
   closeDialog,
