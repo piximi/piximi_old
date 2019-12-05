6b07170f07da8043d1724fc579dd063418232713
Allen Goodman <allen.goodman@icloud.com>
2019-12-05 11:30:58 -0500

    UpdateReduxStore

---
+++ packages/@piximi/upload-image-dialog/src/UploadImageDialog/UploadImageDialog/UpdateReduxStore.ts
@@ -0,0 +1,78 @@
+import * as ImageJS from 'image-js';
+import md5 from 'crypto-js/md5';
+import { Plugin, Uppy, UppyFile } from '@uppy/core';
+
+type UpdateReduxStoreOptions = {
+  createImage: (checksum: string, data: string, identifier: string) => void;
+  id?: string;
+};
+
+export class UpdateReduxStore extends Plugin {
+  createImage: (checksum: string, data: string, identifier: string) => void;
+  id: string;
+  type: string;
+
+  constructor(uppy: Uppy, options: UpdateReduxStoreOptions) {
+    super(uppy, options);
+
+    this.createImage = options.createImage;
+    this.id = options.id || 'UpdateReduxStore';
+    this.type = 'uploader';
+
+    this.uploader = this.uploader.bind(this);
+  }
+
+  install() {
+    this.uppy.addUploader(this.uploader);
+  }
+
+  uninstall() {
+    this.uppy.removeUploader(this.uploader);
+  }
+
+  private uploader(identifiers: Array<string>) {
+    identifiers.forEach(identifier => {
+      const file = this.uppy.getFile(identifier);
+
+      this.uppy.emit('preprocess-progress', file, {
+        mode: 'determinate',
+        message: 'preparingUpload',
+        value: 0
+      });
+    });
+
+    const updateStore = (file: UppyFile) => {
+      const data: string = URL.createObjectURL(file.data);
+
+      return ImageJS.Image.load(data).then((image: ImageJS.Image) => {
+        const data: string = image.toDataURL();
+
+        const checksum: string = md5(data).toString();
+
+        this.createImage(checksum, data, '');
+      });
+    };
+
+    const promises = identifiers.map(identifier => {
+      const image: UppyFile = this.uppy.getFile(identifier);
+
+      const emit = () => {
+        this.uppy.emit('preprocess-progress', image, {
+          mode: 'determinate',
+          message: 'preparingUpload',
+          value: 1
+        });
+      };
+
+      return updateStore(image).then(emit);
+    });
+
+    return Promise.all(promises).then(() => {
+      identifiers.forEach(identifier => {
+        const file = this.uppy.getFile(identifier);
+
+        this.uppy.emit('preprocess-complete', file);
+      });
+    });
+  }
+}
--- packages/@piximi/upload-image-dialog/src/UploadImageDialog/UploadImageDialog/UploadImageDialog.tsx
+++ packages/@piximi/upload-image-dialog/src/UploadImageDialog/UploadImageDialog/UploadImageDialog.tsx
@@ -3,50 +3,7 @@ import '@uppy/dashboard/dist/style.css';
 import * as React from 'react';
 import Uppy from '@uppy/core';
 import { DashboardModal } from '@uppy/react';
-import { IndexedDB } from './IndexedDB';
-
-type ReduxOptions = {
-  id?: string;
-  createImage: (checksum: string, data: string, identifier: string) => void;
-};
-
-class UpdateRedux extends Uppy.Plugin {
-  createImage: (checksum: string, data: string, identifier: string) => void;
-
-  constructor(uppy: Uppy.Uppy, options: ReduxOptions) {
-    super(uppy, options);
-
-    this.createImage = options.createImage;
-    this.id = options.id || 'UpdateRedux';
-    this.type = 'modifier';
-
-    this.create = this.create.bind(this);
-
-    this.postprocessor = this.postprocessor.bind(this);
-  }
-
-  create(image: Uppy.UppyFile) {
-    this.createImage('', '', '');
-  }
-
-  postprocessor(identifiers: Array<string>) {
-    const promises = identifiers.map(identifier => {
-      const file = this.uppy.getFile(identifier);
-
-      this.create(file);
-    });
-
-    return Promise.all(promises);
-  }
-
-  install() {
-    this.uppy.addPostProcessor(this.postprocessor);
-  }
-
-  uninstall() {
-    this.uppy.removePreProcessor(this.postprocessor);
-  }
-}
+import { UpdateReduxStore } from './UpdateReduxStore';
 
 type UploadImageDialogProps = {
   closeDialog: () => void;
@@ -61,17 +18,11 @@ export const UploadImageDialog = ({
 }: UploadImageDialogProps) => {
   const uppy = new Uppy.Uppy();
 
-  uppy.use(IndexedDB, {
-    databaseName: 'piximi-images',
-    tableName: 'images',
-    versionNumber: 1
-  });
-
-  uppy.use(UpdateRedux, {
+  uppy.use(UpdateReduxStore, {
     createImage: createImage
   });
 
-  const plugins: Array<string> = ['IndexedDB', 'UpdateRedux'];
+  const plugins: Array<string> = ['UpdateReduxStore'];
 
   return (
     <DashboardModal
