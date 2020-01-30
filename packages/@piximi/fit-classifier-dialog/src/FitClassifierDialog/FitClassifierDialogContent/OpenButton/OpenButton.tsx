c62c17aa4c79c427f8d9be9eac24f60411c27fa4
Levin Moser <levin.moser@outlook.com>
2020-01-30 20:01:05 +0100

    fix hard coded number of classes

--- packages/@piximi/fit-classifier-dialog/src/FitClassifierDialog/FitClassifierDialogContent/OpenButton/OpenButton.tsx
+++ packages/@piximi/fit-classifier-dialog/src/FitClassifierDialog/FitClassifierDialogContent/OpenButton/OpenButton.tsx
@@ -2,18 +2,26 @@ import Button from "@material-ui/core/Button/Button";
 import {openAction} from "@piximi/store";
 import * as React from "react";
 import {useCallback} from "react";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {categoriesCountSelector} from "@piximi/store";
 
 import {useStyles} from "./OpenButton.css";
+
+type OpenButtonProps = {
+  next: any;
+};
 
 const pathname =
   "https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json";
 
-export const OpenButton = ({next}: {next: any}) => {
+export const OpenButton = (props: OpenButtonProps) => {
+  const {next} = props;
+
   const dispatch = useDispatch();
+  const catagoriesCount = useSelector(categoriesCountSelector);
 
   const onClick = useCallback(() => {
-    const payload = {pathname: pathname, classes: 10, units: 100};
+    const payload = {pathname: pathname, classes: catagoriesCount, units: 100};
 
     dispatch(openAction(payload));
 
