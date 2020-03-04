219dc625b5004ad135bf9a3f403c048306ce4e0f
Allen Goodman <allen.goodman@icloud.com>
2020-03-04 09:19:44 -0500

    wrapped gallery-dialog with dialog

--- packages/piximi/src/App/App.tsx
+++ packages/piximi/src/App/App.tsx
@@ -4,22 +4,29 @@ import {makeStyles, ThemeProvider} from "@material-ui/styles";
 import HTML5Backend from "react-dnd-html5-backend";
 import {DndProvider} from "react-dnd";
 import {styles} from "./App.css";
-import {useDrawer} from "@piximi/hooks";
+import {useDialog, useDrawer} from "@piximi/hooks";
 import {NavigationDrawer} from "@piximi/navigation-drawer";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import classNames from "classnames";
 import {ConnectedGalleryDialog} from "@piximi/gallery-dialog";
+import {useEffect} from "react";
 
 const theme = createMuiTheme();
 
 const useStyles = makeStyles(styles);
 
 export const App = () => {
+  const {closeDialog, openDialog, openedDialog} = useDialog();
+
   const {openedDrawer, toggleDrawer} = useDrawer();
 
   const classes = useStyles({});
+
+  useEffect(() => {
+    openDialog();
+  });
 
   return (
     <ThemeProvider theme={theme}>
@@ -35,7 +42,12 @@ export const App = () => {
 
           <NavigationDrawer toggled={openedDrawer} toggle={toggleDrawer} />
 
-          <ConnectedGalleryDialog />
+          <ConnectedGalleryDialog
+            onClose={closeDialog}
+            open={openedDialog}
+            openedDrawer={openedDrawer}
+            toggleDrawer={toggleDrawer}
+          />
         </div>
       </DndProvider>
     </ThemeProvider>
