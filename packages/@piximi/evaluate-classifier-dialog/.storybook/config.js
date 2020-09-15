0ae32dea46fef1daf00f91a4b5f55bd19aadc1ed
Allen Goodman <allen.goodman@icloud.com>
2020-09-15 16:05:33 -0400

    updated package.json

--- packages/@piximi/evaluate-classifier-dialog/.storybook/config.js
+++
@@ -1,7 +0,0 @@
-import {configure} from "@storybook/react";
-
-const req = require.context("../src", true, /\.stories\.(ts|tsx)$/);
-
-configure(() => {
-  req.keys().forEach((filename) => req(filename));
-}, module);
